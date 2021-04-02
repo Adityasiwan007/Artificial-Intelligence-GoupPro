e636dfee3f314168ddb543deb6277be2d5c483e8
NeelSagar39 <neelsagar39@gmail.com>
2021-04-02 18:29:19 +0100

    Snake Game Updated for actions implementation

--- main.py
+++ main.py
@@ -1,121 +1,136 @@
-import sys,random,time
+
 import pygame
-
+import random
 pygame.init()
+import Qlearning
+import itertools
+import json
 
-WIN_X = 500
-WIN_Y = 500
-WIN = pygame.display.set_mode((WIN_X,WIN_Y))
-pygame.display.set_caption('snake game')
-font=pygame.font.SysFont('comicsans',40)
+BLOCK_SIZE = 10 
+DIS_WIDTH = 400
+DIS_HEIGHT = 400
 
-#main function
-def main_menu():
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                main()
-        WIN.fill((0,0,0))
-        main_menu_message = font.render('Press anywhere to start the game' , True , (255,255,255))
-        font_pos = main_menu_message.get_rect(center=(WIN_X//2, WIN_Y//2))
-        WIN.blit(main_menu_message , font_pos)
+
+def GameLoop():
+    global dis
+    
+    dis = pygame.display.set_mode((DIS_WIDTH, DIS_HEIGHT))
+    pygame.display.set_caption('Snake')
+    clock = pygame.time.Clock()
+
+    # Starting position of snake
+    x1 = DIS_WIDTH / 2
+    y1 = DIS_HEIGHT / 2
+    x1_change = 0
+    y1_change = 0
+    snake_list = [(x1,y1),[x1-BLOCK_SIZE , y1],[x1-(2*BLOCK_SIZE) , y1]]
+    length_of_snake = 1
+
+    # Create first food
+    foodx = round(random.randrange(0, DIS_WIDTH - BLOCK_SIZE) / 10.0) * 10.0
+    foody = round(random.randrange(0, DIS_HEIGHT - BLOCK_SIZE) / 10.0) * 10.0
+
+    dead = False
+    reason = None
+    first_time = True
+    while not dead:
+        # Get action from agent
+        action = learner.act(snake_list, (foodx,foody))
+        # action = None
+        events = pygame.event.get()
+        if first_time:
+            x1_change = -BLOCK_SIZE
+            first_time = False
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT or action =='left':
+                    x1_change = -BLOCK_SIZE
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT or action == "right":
+                    x1_change = BLOCK_SIZE
+                    y1_change = 0
+                elif event.key == pygame.K_UP or action == "up":
+                    y1_change = -BLOCK_SIZE
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN or action == "down":
+                    y1_change = BLOCK_SIZE
+                    x1_change = 0
+
+        # Move snake
+        x1 += x1_change
+        y1 += y1_change
+        snake_head = (x1,y1)
+        snake_list.append(snake_head)
+
+        # Check if snake is off screen
+        if x1 >= DIS_WIDTH or x1 < 0 or y1 >= DIS_HEIGHT or y1 < 0:
+            reason = 'Screen'
+            dead = True
+
+        # Check if snake hit tail
+        if snake_head in snake_list[:-1]:
+            reason = 'Tail'
+            dead = True
+        # for square in snake_list[:-1]:
+        #     print(snake_list[:-1],snake_list)
+        #     if pygame.Rect(square[0],square[1],10,10).colliderect(pygame.Rect(snake_head[0],snake_head[1],10,10)):
+        #         reason = 'Tail'
+        #         dead = True
+
+        # Check if snake ate food
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, DIS_WIDTH - BLOCK_SIZE) / 10.0) * 10.0
+            foody = round(random.randrange(0, DIS_HEIGHT - BLOCK_SIZE) / 10.0) * 10.0
+            length_of_snake += 1
+
+        # Delete the last cell since we just added a head for moving, unless we ate a food
+        if len(snake_list) > length_of_snake:
+            del snake_list[0]
+
+        # Draw food, snake and update score
+        dis.fill((0,0,255))
+        DrawFood(foodx, foody)
+        DrawSnake(snake_list)
+        DrawScore(length_of_snake - 1)
         pygame.display.update()
 
+        # Update Q Table
+        learner.UpdateQValues(reason)
+        
+        # Next Frame
+        clock.tick(15)
+    if dead:
+        print(reason)
 
-def game_over(score):
-        while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            WIN.fill((0,0,0))
-            game_over_message = font.render('You Lost' , True , (255,0,0))
-            game_over_score = font.render(f'Your Score was {score}' , True , (255,255,255))
+    return length_of_snake - 1, reason
 
-            font_pos_message = game_over_message.get_rect(center=(WIN_X//2, WIN_Y//2))
-            font_pos_score = game_over_score.get_rect(center=(WIN_X//2, WIN_Y//2+40))
-            WIN.blit(game_over_message , font_pos_message)
-            WIN.blit(game_over_score , font_pos_score)
-            pygame.display.update()
-            time.sleep(3)
-            main_menu()
+def DrawFood(foodx, foody):
+    pygame.draw.rect(dis, (0,255,0), [foodx, foody, BLOCK_SIZE, BLOCK_SIZE])   
 
+def DrawScore(score):
+    font = pygame.font.SysFont("comicsansms", 35)
+    value = font.render(f"Score: {score}", True, (255,255,0))
+    dis.blit(value, [0, 0])
 
-def main():
-    CLOCK = pygame.time.Clock()
-    snake_pos=[200,70]
-    snake_body=[[200,70] , [200-10 , 70] , [200-(2*10),70]]
-    fruit_pos = [0,0]
-    fruit_spawn = True
-    direction = 'right'
-    score=0
-    CLOCK = pygame.time.Clock()
-    #game loop
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+def DrawSnake(snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, (0,0,0), [x[0], x[1], BLOCK_SIZE, BLOCK_SIZE])
 
-            keys= pygame.key.get_pressed()
-            if (keys[pygame.K_w] or keys[pygame.K_UP]) and direction != 'down':
-                direction = 'up'
-            if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and direction != 'up':
-                direction = 'down'
-            if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and direction != 'left':
-                direction = 'right'
-            if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and direction != 'right':
-                direction = 'left'
+# GameLoop()
 
-        WIN.fill((0,0,0))
-        for square in snake_body:
-            if snake_body.index(square) == len(snake_body)-1:
-                pygame.draw.rect(WIN ,(255, 0, 0), (square[0],square[1],20,20))
-            else:
-                pygame.draw.rect(WIN ,(255, 255, 0), (square[0],square[1],20,20))
-            
-        if direction == 'right':
-            snake_pos[0] += 10
-        elif direction == 'left':
-            snake_pos[0] -= 10
-        elif direction == 'up':
-            snake_pos[1] -= 10
-        elif direction == 'down':
-            snake_pos[1] += 10
+game_count = 1
 
-        snake_body.append(list(snake_pos))
+learner = Qlearning.Learner(DIS_WIDTH, DIS_HEIGHT, BLOCK_SIZE)
 
-        if fruit_spawn:
-            fruit_pos = [random.randrange(40,WIN_X-40),random.randrange(40,WIN_Y-40)]
-            fruit_spawn = False
-
-        pygame.draw.rect(WIN ,(138,43,226),(fruit_pos[0],fruit_pos[1],20,20))
-
-        score_font = font.render(f'{score}' , True , (255,255,255))
-        font_pos = score_font.get_rect(center=(WIN_X//2-40 , 30))
-        WIN.blit(score_font , font_pos)
-
-        if pygame.Rect(snake_pos[0],snake_pos[1],20,20).colliderect(pygame.Rect(fruit_pos[0],fruit_pos[1],20,20)):
-            fruit_spawn=True
-            score += 5
-        else:
-            snake_body.pop(0)
-
-        for square in snake_body[:-1]:
-            if pygame.Rect(square[0],square[1],10,10).colliderect(pygame.Rect(snake_pos[0],snake_pos[1],10,10)):
-                game_over(score)
-
-        if snake_pos[0]+10 <=0 or snake_pos[0] >= WIN_X:
-            game_over(score)
-        if snake_pos[1]+10 <=0 or snake_pos[1] >= WIN_Y:
-            game_over(score)
-        pygame.display.update()
-
-        CLOCK.tick(5)
-
-#caliing the main function
-main_menu()
-
+while True:
+    learner.Reset()
+    if game_count > 100:
+        learner.epsilon = 0
+    else:
+        learner.epsilon = .1
+    score, reason = GameLoop()
+    print(f"Games: {game_count}; Score: {score}; Reason: {reason}") # Output results of each game to console to monitor as agent is training
+    game_count += 1
+    if game_count % 100 == 0: # Save qvalues every qvalue_dump_n games
+        print("Save Qvals")
+        learner.SaveQvalues()
