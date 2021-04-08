4969875db7ca024e4e520ee395c3723888d0961a
Vishal Uchil <vishaluchil99@outlook.com>
2021-04-08 18:15:58 +0100

    Breadth First Search Implemented

---
+++ BreadthFirstSearch.py
@@ -0,0 +1,91 @@
+class ShortestPathBFSSolver(object):
+
+    def __init__(self, display_width, display_height, block_size):
+        # BaseGameModel.__init__(self, "Shortest Path BFS", "shortest_path_bfs", "spb")
+        # Game parameters
+        self.display_width = display_width
+        self.display_height = display_height
+        self.block_size = block_size
+
+        # Action space
+        self.actions = {
+            0:'left',
+            1:'right',
+            2:'up',
+            3:'down'
+        }
+
+    def move(self, snake_list, fruit_node):
+        # BaseGameModel.move(self, environment)
+        # shortest_path_move_from_transposition_table = self._path_move_from_transposition_table(self.starting_node, self.fruit_node)
+        # if shortest_path_move_from_transposition_table:
+        #     return shortest_path_move_from_transposition_table
+        self.snake_list = snake_list
+        self.fruit_node = fruit_node
+        self.starting_node = snake_list[-1]
+        # print(self.starting_node)
+
+        shortest_path = self.shortest_path(self.starting_node, self.fruit_node, self.snake_list[1::])
+        if shortest_path:
+            # self.transposition_table[self.fruit_node] = shortest_path
+            # first_point = shortest_path[-2]
+            return shortest_path
+        return None
+
+    def possible_action(self, starting_node, snake_list):
+        # distance_x_right = self.display_width - (starting_node[0] + self.block_size)
+        # distance_x_left = self.display_width - distance_x_right
+        # distance_y_up = self.display_height - (starting_node[1] + self.block_size)
+        # distance_y_down = self.display_height - distance_y_up
+        pos_actions = [[0],[0],[0],[0]]
+        
+        
+        if (starting_node[0]+self.block_size < self.display_width and [starting_node[0]+self.block_size, starting_node[1]] not in snake_list):
+            pos_actions[3] = [[starting_node[0]+self.block_size, starting_node[1]],['right']]
+        else:
+            pos_actions[3] = [0]
+        if(starting_node[0]-self.block_size > 0 and [starting_node[0]-self.block_size, starting_node[1]] not in snake_list): 
+            pos_actions[2] = [[starting_node[0]-self.block_size, starting_node[1]],['left']]
+        else:
+            pos_actions[2] = [0]
+        if (starting_node[1]+self.block_size < self.display_height and [starting_node[0], starting_node[1]+self.block_size] not in snake_list):
+            pos_actions[1] = [[starting_node[0], starting_node[1]+self.block_size],['down']]
+        else:
+            pos_actions[1] = [0]
+        if (starting_node[1]-self.block_size > 0 and [starting_node[0], starting_node[1]-self.block_size] not in snake_list):
+            pos_actions[0] = [[starting_node[0], starting_node[1]-self.block_size],['up']]
+        else:
+            pos_actions[0] = [0]
+        # for body in self.snake_list:
+        #     if body['x'] == x and body['y'] == y:
+        #         return True
+        #     return False
+        #print('ssdasd',pos_actions)
+        return pos_actions
+
+
+    def shortest_path(self, start, end, snake_list):
+        queue = [[start,[]]]
+        # print(snake_list)
+        visited_nodes = [list(x) for x in snake_list]
+        #visited_nodes = snake_list
+        # print(visited_nodes)
+        shortest_path = []
+        while queue:
+            current_node = queue.pop(0)
+            # print(current_node[0])
+            path = current_node[1]
+            #print(path)
+            if (current_node[0] == end):
+                # print("sdasdsakdsalkdlsaldkasl;d;lasl;das;ld",path)
+                return path
+            if current_node[0] not in visited_nodes:
+                visited_nodes.append([current_node[0][0],current_node[0][1]])
+                # print(visited_nodes)
+                for action in self.possible_action(current_node[0], visited_nodes):
+                # print(action)
+                    if action[0] == 0:
+                        continue
+                    new_path = path + [action[1]]
+                    queue.append([action[0],new_path])
+                
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-
 import pygame
 import random
 pygame.init()
 import Qlearning
 import itertools
 import json
+import BreadthFirstSearch
 
 BLOCK_SIZE = 10 
 DIS_WIDTH = 400
 DIS_HEIGHT = 400
 ticks = 100
 
-def GameLoop():
+def Q_GameLoop(learner):
     global dis
     
     dis = pygame.display.set_mode((DIS_WIDTH, DIS_HEIGHT))
@@ -128,21 +128,151 @@ def DrawSnake(snake_list):
         pygame.draw.rect(dis, (0,0,0), [x[0], x[1], BLOCK_SIZE, BLOCK_SIZE])
 
 # GameLoop()
+def run_q_learning():
+    game_count = 1
 
-game_count = 1
+    learner = Qlearning.Learner(DIS_WIDTH, DIS_HEIGHT, BLOCK_SIZE)
 
-learner = Qlearning.Learner(DIS_WIDTH, DIS_HEIGHT, BLOCK_SIZE)
+    while True:
+        learner.Reset()
+        if game_count > 100:
+            learner.epsilon = 0
+        else:
+            learner.epsilon = .1
+        score, reason = Q_GameLoop(learner)
+        print(f"Games: {game_count}; Score: {score}; Reason: {reason}") # Output results of each game to console to monitor as agent is training
+        game_count += 1
+        if game_count % 100 == 0: # Save qvalues every qvalue_dump_n games
+            print("Save Qvals")
+            learner.SaveQvalues()
 
-while True:
-    learner.Reset()
-    if game_count > 100:
-        learner.epsilon = 0
-    else:
-        learner.epsilon = .1
-    score, reason = GameLoop()
-    print(f"Games: {game_count}; Score: {score}; Reason: {reason}") # Output results of each game to console to monitor as agent is training
-    game_count += 1
-    if game_count % 100 == 0: # Save qvalues every qvalue_dump_n games
-        print("Save Qvals")
-        learner.SaveQvalues()
+def bfs_Gameloop(bfs):
+    global dis
     
+    dis = pygame.display.set_mode((DIS_WIDTH, DIS_HEIGHT))
+    pygame.display.set_caption('Snake')
+    clock = pygame.time.Clock()
+
+    # Starting position of snake
+    x1 = DIS_WIDTH / 2
+    y1 = DIS_HEIGHT / 2
+    x1_change = 0
+    y1_change = 0
+    snake_list = [(x1,y1)]
+    length_of_snake = 1
+
+    # Create first food
+    foodx = round(random.randrange(0, DIS_WIDTH - BLOCK_SIZE) / 10.0) * 10.0
+    foody = round(random.randrange(0, DIS_HEIGHT - BLOCK_SIZE) / 10.0) * 10.0
+
+    dead = False
+    reason = None
+    first_time = True
+    
+        # Get action from agent
+        # action = learner.act(snake_list, (foodx,foody))
+    
+    # print(actions)
+    prev_action = None
+    while not dead:
+        
+        actions = bfs.move(snake_list, [foodx,foody])
+        while (actions == None):
+            foodx = round(random.choice([i for i in range(0,DIS_WIDTH-BLOCK_SIZE) if i not in snake_x]) / 10.0) * 10.0
+            foody = round(random.choice([i for i in range(0,DIS_HEIGHT-BLOCK_SIZE) if i not in snake_y]) / 10.0) * 10.0
+            actions = bfs.move(snake_list, [foodx,foody])
+        for action in actions:
+            event = pygame.event.get()
+            dis.fill((0,0,255))
+            # action = None
+            # events = pygame.event.get()
+            # if first_time:
+            #     x1_change = -BLOCK_SIZE
+                #first_time = False
+            # for event in events:
+            #     if event.type == pygame.KEYDOWN:
+            #         if event.key == pygame.K_LEFT or action =='left':
+            #             x1_change = -BLOCK_SIZE
+            #             y1_change = 0
+            #         elif event.key == pygame.K_RIGHT or action == "right":
+            #             x1_change = BLOCK_SIZE
+            #             y1_change = 0
+            #         elif event.key == pygame.K_UP or action == "up":
+            #             y1_change = -BLOCK_SIZE
+            #             x1_change = 0
+            #         elif event.key == pygame.K_DOWN or action == "down":
+            #             y1_change = BLOCK_SIZE
+            #             x1_change = 0
+            #print(action)
+            if action[0] =='left':
+                x1_change = -BLOCK_SIZE
+                y1_change = 0
+                prev_action = 'left'
+            elif action[0] == "right":
+                x1_change = BLOCK_SIZE
+                y1_change = 0
+                prev_action = 'right'
+            elif action[0] == "up":
+                y1_change = -BLOCK_SIZE
+                x1_change = 0
+                prev_action = 'up'
+            elif action[0] == "down":
+                y1_change = BLOCK_SIZE
+                x1_change = 0
+                prev_action = 'down'
+            # Move snake
+            x1 += x1_change
+            y1 += y1_change
+            snake_head = (x1,y1)
+            snake_list.append(snake_head)
+            
+            DrawFood(foodx, foody)
+            DrawSnake(snake_list)
+            DrawScore(length_of_snake - 1)
+            pygame.display.update()
+            # Check if snake is off screen
+            if x1 >= DIS_WIDTH or x1 < 0 or y1 >= DIS_HEIGHT or y1 < 0:
+                reason = 'Screen'
+                dead = True
+
+            # Check if snake hit tail
+            if snake_head in snake_list[:-1]:
+                reason = 'Tail'
+                dead = True
+            # for square in snake_list[:-1]:
+            #     print(snake_list[:-1],snake_list)
+            #     if pygame.Rect(square[0],square[1],10,10).colliderect(pygame.Rect(snake_head[0],snake_head[1],10,10)):
+            #         reason = 'Tail'
+            #         dead = True
+
+            # Check if snake ate food
+            if x1 == foodx and y1 == foody:
+                snake_x = []
+                snake_y = []
+                for snake in snake_list:
+                    snake_x.append(snake[0]) 
+                    snake_y.append(snake[1]) 
+                foodx = round(random.choice([i for i in range(0,DIS_WIDTH-BLOCK_SIZE) if i not in snake_x]) / 10.0) * 10.0
+                foody = round(random.choice([i for i in range(0,DIS_HEIGHT-BLOCK_SIZE) if i not in snake_y]) / 10.0) * 10.0
+                # if ((new_foodx,new_foody) in snake_list):
+                #     print(True)
+                #     while((new_foodx,new_foody) in snake_list):
+                #         new_foodx = round(random.randrange(0, DIS_WIDTH - BLOCK_SIZE) / 10.0) * 10.0
+                #         new_foody = round(random.randrange(0, DIS_HEIGHT - BLOCK_SIZE) / 10.0) * 10.0
+                # foodx=new_foodx
+                # foody=new_foody
+                length_of_snake += 1
+
+            # Delete the last cell since we just added a head for moving, unless we ate a food
+            if len(snake_list) > length_of_snake:
+                del snake_list[0]
+
+            # Next Frame
+            clock.tick(50)
+    if dead:
+        print(reason)
+
+def run_bfs():
+    bfs = BreadthFirstSearch.ShortestPathBFSSolver(DIS_WIDTH, DIS_HEIGHT, BLOCK_SIZE)
+    bfs_Gameloop(bfs)
+run_bfs()
